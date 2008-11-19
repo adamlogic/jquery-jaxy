68f1f7abb712d5ef76dede5b8e91112626bee81b
Adam McCrea <adam@adamlogic.com>
2008-11-19 10:13:23 -0500

    add vals for setting multiple values in a form

--- jquery.jaxy.js
+++ jquery.jaxy.js
@@ -4,8 +4,8 @@ $.fn.extend({
 
   jaxy: function() {
     return this.each(function() {
-      if (/^a$/.test(this.tagName)) $(this).ajaxLink();
-      if (/^form$/.test(this.tagName)) $(this).ajaxForm({dataType: 'script'});
+      if (/^a$/i.test(this.tagName)) $(this).ajaxLink();
+      if (/^form$/i.test(this.tagName)) $(this).ajaxForm({dataType: 'script'});
     });
   },
 
@@ -27,6 +27,13 @@ $.fn.extend({
     return this.change(function() {
       $(this).parents('form:first').submit();
     });
+  },
+  
+  vals: function(vals) {
+    for (var name in vals) {
+      this.find('[name="' + name + '"]').val(vals[name]);
+    }
+    return this;
   }
 
 });
