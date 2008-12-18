6440a70c9c501a7cb57c615d5061c4e15cae3721
Adam McCrea <adam@adamlogic.com>
2008-12-18 08:31:45 -0500

    assist rails with ajax file uploads

--- jquery.jaxy.js
+++ jquery.jaxy.js
@@ -4,16 +4,24 @@ $.fn.extend({
 
   jaxy: function() {
     return this.each(function() {
-      if (/^a$/i.test(this.tagName)) $(this).ajaxLink();
-      if (/^form$/i.test(this.tagName)) $(this).ajaxForm({dataType: 'script'});
+      if (/^a$/i.test(this.tagName)) $(this).jaxyLink();
+      if (/^form$/i.test(this.tagName)) $(this).jaxyForm();
     });
   },
 
-  ajaxLink: function() {
+  jaxyLink: function() {
     return this.click(function() {
       $.getScript(this.href);
       return false;
     });
+  },
+
+  jaxyForm: function() {
+    // ajax file uploads use an iframe hack, so tell the server we want a js response
+    if (/multipart/.test(this.attr('enctype'))) {
+      this.append('<input type="hidden" name="format" value="js" />');
+    }
+    return this.ajaxForm({dataType: 'script'})
   },
 
   submitClick: function() {
